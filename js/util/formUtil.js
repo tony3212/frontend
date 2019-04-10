f3199cecd15c3493c551302d563d78c5b2448747
qfzhu <qfzhu@onlyou.com>
2019-04-10 10:32:07 +0800

    develop

--- js/util/formUtil.js
+++ js/util/formUtil.js
@@ -4,6 +4,27 @@
  */
 var formUtil = formUtil || {
     /**
+     * 转义html
+     * @param string
+     * @returns {string}
+     */
+    escapeHtml: function (string) {
+        var htmlEscapes = {
+                '&': '&amp;',
+                '<': '&lt;',
+                '>': '&gt;',
+                '"': '&quot;',
+                "'": '&#x27;',
+                '/': '&#x2F;'
+            },
+            htmlEscaper = /[&<>"'\/]/g;
+
+        return ('' + string).replace(htmlEscaper, function (match) {
+            return htmlEscapes[match];
+        });
+    },
+
+    /**
      * 根据要提交的数据对象生成一个form表单，并提交表单，提交完成后删除该表单
      * @param formObject 要提交的对象
      * @param formAttribute 表单配制， {id: ..., method: "", target}
@@ -18,10 +39,10 @@ var formUtil = formUtil || {
         $.each(formObject, function (fieldName, fieldValue) {
             if ($.isArray(fieldValue)) {
                 $.each(fieldValue, function (index, value) {
-                    $form.append(inputTemplate.replace("{1}", fieldName + "[]").replace("{2}", value));
+                    $form.append(inputTemplate.replace("{1}", fieldName + "[]").replace("{2}", formUtil.escapeHtml(value)));
                 });
             } else {
-                fieldValue != null && $form.append(inputTemplate.replace("{1}", fieldName).replace("{2}", fieldValue));
+                fieldValue != null && $form.append(inputTemplate.replace("{1}", fieldName).replace("{2}", formUtil.escapeHtml(fieldValue)));
             }
         });
 
